26cdcdab4bcd5e994530fe15343e1d8750b00006
ElvisMuresan <elvis.e.muresan@gmail.com>
2023-11-20 00:38:08 +0200

    Finished the logical Operators lesson

--- logicalOperators.js
+++ logicalOperators.js
@@ -17,6 +17,44 @@ let a2 = 1;
 let b2 = -2;
 
 console.log(a2 < 3 || b2 < -2);
-//EX5
 
+//EX5
 console.log("ce va afisa? 👉", 0 || 0 || 0);
+
+//EX6
+function A() {
+  console.log("called A");
+  return true;
+}
+function B() {
+  console.log("called B");
+  return false;
+}
+
+console.log(A() || B());
+
+//EX7
+function A() {
+  console.log("called A");
+  return false;
+}
+function B() {
+  console.log("called B");
+  return true;
+}
+
+console.log(A() && B());
+
+//EX 8
+let foo = null ?? "default string";
+console.log(foo);
+
+//Ex 9
+let score = 0;
+
+console.log(score || "no score registered");
+
+//Ex 10
+let score1 = 0;
+
+console.log(score1 ?? "no score registered");
