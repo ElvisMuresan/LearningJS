2d05179d0bb853dcaadb84b95a4942c66418bd56
ElvisMuresan <elvis.e.muresan@gmail.com>
2023-11-19 23:46:13 +0200

    Created the js file for logical Operator lesson

---
+++ logicalOperators.js
@@ -0,0 +1,22 @@
+//EX1
+let a = 1;
+a = !a;
+console.log(typeof a, a);
+
+//EX2
+let a1 = 1;
+let b = -2;
+
+console.log(a1 < 3 && b < -2);
+
+//EX3
+console.log("***88 && 33 && 5 👉", 88 && 33 && 5);
+
+//EX4
+let a2 = 1;
+let b2 = -2;
+
+console.log(a2 < 3 || b2 < -2);
+//EX5
+
+console.log("ce va afisa? 👉", 0 || 0 || 0);
