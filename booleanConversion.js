32332915eef59270870ca18fe125feb55a8246ae
ElvisMuresan <elvis.e.muresan@gmail.com>
2023-11-20 18:59:51 +0200

    Finished the boolean conversion lesson

---
+++ booleanConversion.js
@@ -0,0 +1,34 @@
+//EX1
+let age = 0;
+let hasAge = Boolean(age);
+
+console.log(hasAge);
+
+//EX2
+let age1 = 1;
+let hasAge1 = !!age1;
+
+console.log(hasAge1);
+
+//EX3
+let x = 8;
+console.log(x, typeof x);
+x = x + "";
+console.log(x, typeof x);
+x = x - 2;
+console.log(x, typeof x);
+
+//Ex4
+let elvis = 3;
+console.log(elvis, typeof elvis);
+elvis = elvis + "";
+console.log(elvis, typeof elvis);
+elvis = elvis - 2;
+console.log(elvis, typeof elvis);
+
+//Ex5
+let ana = " ";
+console.log(ana, typeof ana);
+let bana = Number(ana);
+console.log(bana, typeof bana);
+console.log(ana, typeof ana);
