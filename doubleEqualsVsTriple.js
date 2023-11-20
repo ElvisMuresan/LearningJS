72b2e0e8956f8ee4631ed6b13b27af2b93793247
ElvisMuresan <elvis.e.muresan@gmail.com>
2023-11-20 21:50:22 +0200

    Finished the doubleEqual lesson

---
+++ doubleEqualsVsTriple.js
@@ -0,0 +1,24 @@
+//EX1
+const a = 100;
+const b = "100";
+
+console.log(a == b);
+
+//EX2
+let a1 = null;
+let b1 = 10;
+
+console.log(a1 == b1);
+
+//EX3
+let a2 = null;
+let b2;
+
+console.log(a2 == b2);
+
+//EX 4
+
+let a3 = 100;
+let b3 = 100;
+
+console.log(a3 === b3);
