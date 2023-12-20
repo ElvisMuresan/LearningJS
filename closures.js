2e332b18c639e67874130ac6f659795954f4084c
ElvisMuresan <elvis.e.muresan@gmail.com>
2023-12-20 19:43:19 +0200

    Added some example to the closure lesson

--- closures.js
+++ closures.js
@@ -24,3 +24,36 @@ function foo() {
 
 let number = foo();
 number();
+
+//EX3
+function initial() {
+  let name = "Elvis";
+  function displayName() {
+    console.log(name);
+  }
+  displayName();
+}
+initial();
+
+//EX4
+function initial() {
+  let name = "Elvis";
+  function displayName() {
+    console.log(name);
+  }
+  return displayName;
+}
+const result = initial();
+result();
+
+//EX5
+function makeSum(a) {
+  return function sum(b) {
+    return a + b;
+  };
+}
+
+const add10 = makeSum(10);
+const add20 = makeSum(20);
+console.log(add10(5));
+console.log(add20(5));
