819d84506dcc271c747e4a24619363e237bee615
ElvisMuresan <elvis.e.muresan@gmail.com>
2024-01-11 14:46:42 +0200

    Finished the call, bind and apply lesson

--- callBindApply.js
+++ callBindApply.js
@@ -87,3 +87,51 @@ const fotballll = {
 //console.log(fotball.fotballPlayer())
 let fotballPlayer = fotballll.fotballPlayer.bind(fotballl);
 setTimeout(fotballPlayer);
+
+//EX5 `arguments` array
+function func1(a, b, c) {
+  console.log(arguments[0]);
+  console.log(arguments[1]);
+  console.log(arguments[2]);
+}
+
+func1(1, 2, 3);
+
+//EX6
+function Car(type, fuelType) {
+  this.type = type;
+  this.fuelType = fuelType;
+  console.log(this);
+}
+
+function setBrand(brand) {
+  Car.apply(this, ["convertible", "petrol"]);
+  this.brand = brand;
+  console.log(`Car details= `, this);
+}
+
+function definePrice(price) {
+  Car.apply(this, new Array("convertible", "diesel"));
+  this.price = price;
+  console.log(`Car details= `, this);
+}
+
+const newBrand = new setBrand("Brand1");
+const newCarPrice = new definePrice(10000);
+
+//EX7
+function addUp() {
+  //Using arguments to capture the arbitrary number of inputs
+  const args = Array.from(arguments);
+  this.x = args.reduce((prev, curr) => prev + curr, 0);
+  console.log("this.x = ", this.x);
+}
+
+function driverFunc() {
+  const obj = {
+    inps: [1, 2, 3, 4, 5, 6],
+  };
+  addUp.apply(obj, obj.inps);
+}
+
+driverFunc();
