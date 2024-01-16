eab3d84a88e2a06217f3fbc6177a9d18688b7e59
ElvisMuresan <elvis.e.muresan@gmail.com>
2024-01-16 22:09:11 +0200

    Added an example to the eventLoop lesson

--- eventLoop.js
+++ eventLoop.js
@@ -13,3 +13,19 @@ function printSquare(n) {
 }
 
 printSquare(10);
+
+//Synchronous
+[1, 2, 3, 4].forEach(function (i) {
+  console.log(i);
+});
+
+//Asynchronous
+function asyncForEach(array, cb) {
+  array.forEach(function () {
+    setTimeout(cb, 0);
+  });
+}
+
+asyncForEach([1, 2, 3, 4], function (i) {
+  console.log("processing asyc", i);
+});
