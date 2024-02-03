b60aeaa90c6eb7665082a1ba1a2950c66189d4c4
ElvisMuresan <elvis.e.muresan@gmail.com>
2024-02-03 23:08:56 +0200

    Added some example to the async await lesson

--- async.await.js
+++ async.await.js
@@ -11,3 +11,45 @@ async function add(x, y) {
 add(2, 3).then((result) => {
   console.log(result);
 });
+
+//ex3
+function resolvedAfter2Seconds() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("resolved");
+    }, 2000);
+  });
+}
+
+async function asyncCall() {
+  console.log("calling");
+  const result = await resolvedAfter2Seconds();
+  console.log(result);
+}
+
+asyncCall();
+//ex4
+function myFunction(number) {
+  return new Promise((resolve, reject) => {
+    if (number > 10) {
+      resolve("Number e mai mare decat 10");
+    } else {
+      reject("Number e mai mic decat 10");
+    }
+  });
+}
+// myFunction(9)
+//     .then((success) => {console.log(success)})
+//     .catch((error) => {console.log(error)})
+
+//Resolve with async await
+async function asyncCall() {
+  try {
+    const result = await myFunction(11);
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+asyncCall();
